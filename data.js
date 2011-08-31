bddfdd990f7eb0c24ce81c0b19c155c6be10beea
Rob <byroweb-mail@yahoo.it>
2011-08-31 17:07:30 +0100

    Creating packages to wrap code properly

---
+++ data.js
@@ -0,0 +1,24 @@
+BYRO.vnstat.data.Data = function()
+{
+
+this.data_daily = [{d: 0, time: "31/8", rx: 58, tx: 2},
+{d: 1, time: "30/8", rx: 19, tx: 3},
+{d: 2, time: "29/8", rx: 31, tx: 2},
+{d: 3, time: "28/8", rx: 47, tx: 4},
+{d: 4, time: "27/8", rx: 63, tx: 4},
+{d: 5, time: "26/8", rx: 0, tx: 0},
+];
+
+this.data_hourly = [{d: 0, time: "31/8", rx: 41, tx: 23},
+{d: 1, time: "30/8", rx: 22, tx: 4},
+{d: 2, time: "29/8", rx: 10, tx: 8},
+{d: 3, time: "28/8", rx: 54, tx: 34},
+{d: 4, time: "27/8", rx: 63, tx: 40},
+{d: 5, time: "26/8", rx: 6, tx: 3},
+];
+
+this.day_entries = 6;
+
+//dataset = dataset_daily;
+
+};
---
+++ data_provider.js
@@ -0,0 +1,4 @@
+BYRO.vnstat.data.DataProvider = function()
+{
+
+};
